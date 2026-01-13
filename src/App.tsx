1397c96b06944a281d9b90194a09cec17198e4d1
Moski <sacamp23@gmail.com>
2026-01-12 17:05:30 -0800

    Hard reset render test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { SignedIn, SignedOut } from "@clerk/clerk-react";
-import Chat from "./Chat";
-import Landing from "./Landing";
-import HousingIndustry from "./pages/industries/Housing";
-import LegalIndustry from "./pages/industries/Legal";
-import OperationsIndustry from "./pages/industries/Operations";
-
-export default function App() {
+function App() {
   return (
     <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/how-it-works" element={<HowItWorks />} />
-          <Route path="/industries" element={<Industries />} />
-          <Route path="/industries/housing" element={<HousingIndustry />} />
-          <Route path="/industries/legal" element={<LegalIndustry />} />
-          <Route path="/industries/operations" element={<OperationsIndustry />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/security" element={<Security />} />
-          <Route path="/pilot" element={<Pilot />} />
-        </Routes>
-        <Footer />
+      <h1>Reasonly is working</h1>
+      <p>If you can read this, React is rendering.</p>
     </>
   );
 }
+
+export default App;
